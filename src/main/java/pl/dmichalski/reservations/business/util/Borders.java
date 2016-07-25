991f525426d1f1339bd7526ac80180f519b8c467
Daniel Michalski <michalskidaniel2@gmail.com>
2016-07-25 10:30:48 +0200

    Little refactoring 2

--- src/main/java/pl/dmichalski/reservations/business/util/Borders.java
+++ src/main/java/pl/dmichalski/reservations/business/util/Borders.java
@@ -5,8 +5,8 @@ import javax.swing.border.Border;
 
 public class Borders {
 
-    private static final int HEIGHT = 20;
     private static final int WIDTH = 40;
+    private static final int HEIGHT = 20;
 
     public static Border createEmptyBorder() {
         return BorderFactory.createEmptyBorder(HEIGHT, WIDTH, HEIGHT, WIDTH);
