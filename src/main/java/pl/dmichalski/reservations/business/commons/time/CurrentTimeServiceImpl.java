cf5469658db6d98be036ebf8f7847679ad17b823
Daniel Michalski <michalskidaniel2@gmail.com>
2020-07-30 10:36:42 +0200

    Change Time service name

--- src/main/java/pl/dmichalski/reservations/business/commons/time/CurrentTimeServiceImpl.java
+++ src/main/java/pl/dmichalski/reservations/business/commons/time/CurrentTimeServiceImpl.java
@@ -5,7 +5,7 @@ import java.util.Date;
 import org.springframework.stereotype.Service;
 
 @Service
-class DefaultCurrentTimeService implements CurrentTimeService {
+class CurrentTimeServiceImpl implements CurrentTimeService {
 
     @Override
     public Date getCurrentDate() {
